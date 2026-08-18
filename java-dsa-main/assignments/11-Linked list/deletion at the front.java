a1e37e099e09b01e9e4dce2e2ed7120b0c4d9daa
Nithish Kumar Reddy <9824005015@klu.ac.in>
2026-08-18 14:53:38 +0530

    Create deletion at the front.java

---
+++ java-dsa-main/assignments/11-Linked list/deletion at the front.java
@@ -0,0 +1,37 @@
+class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+
+        Node second = new Node(20);
+        head.next = second;
+        second.prev = head;
+
+        Node third = new Node(30);
+        second.next = third;
+        third.prev = second;
+
+        if (head != null) {
+            head = head.next;
+
+            if (head != null) {
+                head.prev = null;
+            }
+        }
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
